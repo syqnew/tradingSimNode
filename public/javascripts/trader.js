e8b9f2dc62321e0540088d9e8a2c4e9193961ef0
Stephanie New <syqnew@mit.edu>
2014-02-27 00:29:18 -0500

    Separated the js from the html

--- public/javascripts/trader.js
+++ public/javascripts/trader.js
@@ -1,15 +1,66 @@
-function Socket() {
-	this.socket = io.connect('http://localhost');
-}
+_traderInfoTemplate = Handlebars.compile($('#traderInfo-template').html());
+_tradeTemplate = Handlebars.compile($('#trade-template').html());
+_orderInputsVolumeTemplate = Handlebars.compile($('#orderInputsVolume-template').html());
+_orderInputsBothTemplate = Handlebars.compile($('#orderInputsBoth-template').html());
+_transactionsTemplate = Handlebars.compile($('#transactions-template').html());
+_porfolioTemplate = Handlebars.compile($('#portfolio-template').html());
 
-Socket.prototype.connectToServer = function(name, email, callback) {
-    this.socket.emit('set nickname', {name: name, email: email}); 
-    
-    this.socket.on('ready', function (data) {
-    	callback();
-    });
-};
+$('#traderView').html(_traderInfoTemplate);
+      
+var socket = io.connect('http://localhost');
+//var socket = new Socket()
 
-Socket.prototype.makeOrder = function(orderObject) {
-	this.socket.emit('make order', orderObject);
-};
+$('#submitBtn').click( function() {
+	if ( $('#nameInput').val().length > 0 && $('#emailInput').val().length > 0 ) {
+  		socket.emit('set nickname', {name: $('#nameInput').val() , email: $('#emailInput').val()});
+    	socket.on('ready', function (data) {
+
+	    	$('#traderView').html(_tradeTemplate);
+	    	$('#orderInputs').html(_orderInputsVolumeTemplate);
+	    	$('#transactions').html(_transactionsTemplate({ transactions: ["You sold 100 shares at $2332", "You bought 200 shares at $5"] } ));
+	    	var samplePortfolio = {}
+    		samplePortfolio['last']='34';
+    		samplePortfolio['low']='34';
+    		samplePortfolio['high']='34';
+    		samplePortfolio['bid']='34';
+    		samplePortfolio['bidSize']='34';
+    		samplePortfolio['ask']='34';
+    		samplePortfolio['askSize']='34';
+    		samplePortfolio['volume']='34';
+    		samplePortfolio['quantity']='34';
+    		samplePortfolio['crlTotal']='34';
+			samplePortfolio['cashTotal']='34';
+			samplePortfolio['total']='34';
+
+			$('#portfolio').html(_porfolioTemplate(samplePortfolio));
+
+			$('#orderType').on('change', function() {
+				var option = $(this).val();
+				if (option === 'option1' || option === 'option2') $('#orderInputs').html(_orderInputsVolumeTemplate);
+				else $('#orderInputs').html(_orderInputsBothTemplate);
+    		});
+
+			$('#submitOrderBtn').click(function() {
+				//TODO: check if inputs were valid and non empty
+				var orderObject = {};
+				orderObject['volume'] = $('#volumeInput').val();
+				var option = $('#orderType').val();
+				console.log(option);
+				if (option === 'option1') orderObject['type'] = 'marketBuy';
+				else if (option === 'option2') orderObject['type'] = 'marketSell';
+				else if (option === 'option3') {
+					orderObject['type'] = 'limitBuy';
+					orderObject['price'] = $('#priceInput').val();
+				}
+				else if (option === 'option4') {
+					orderObject['type'] = 'limitSell';
+					orderObject['price'] = $('#priceInput').val();
+				}
+				socket.emit('make order', orderObject);
+				//socket.makeOrder(orderObject);
+				$('input').val('');
+                
+			});
+    	});
+    }
+});
