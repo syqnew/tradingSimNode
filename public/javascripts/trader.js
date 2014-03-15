e1cc06bc2937bbc0283a8b9f2ac4561ed956da23
Stephanie New <syqnew@mit.edu>
2014-03-15 13:58:35 -0400

    Disabled all buttons before and after trading session

--- public/javascripts/trader.js
+++ public/javascripts/trader.js
@@ -17,72 +17,11 @@ $('#submitBtn').click( function() {
 	if ( $('#nameInput').val().length > 0 && $('#emailInput').val().length > 0 ) {
   		socket.emit('set nickname', {name: $('#nameInput').val() , email: $('#emailInput').val()});
     	socket.on('ready', function (data) {
+    		$('#traderView').html(_tradeTemplate);
+    		$('.trading').prop('disabled', true);
+    		// $('#submitOrderBtn').prop('disabled', true);
 
-    		// Initialize the trading panel
-	    	$('#traderView').html(_tradeTemplate);
-	    	$('#orderInputs').html(_orderInputsVolumeTemplate);
-	    	$('#cancelOrders').html(_cancelOrdersTemplate({ orders: ['Buy 13 shares at $324', 'Buy 13 shares at $324', 'Sell 23 shares at $23'] }));
-	    	generateChartData();
-	    	makeChart();
-
-	    	// Get fake transactions
-	    	$('#transactions').html(_transactionsTemplate({ transactions: ["You sold 100 shares at $2332", "You bought 200 shares at $5", 
-	    		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
-	    		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
-	    		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
-	    		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5"] } ));
-
-	    	// Set fake portfolio data
-	    	var samplePortfolio = {}
-    		samplePortfolio['last']='34';
-    		samplePortfolio['low']='34';
-    		samplePortfolio['high']='34';
-    		samplePortfolio['bid']='34';
-    		samplePortfolio['bidSize']='34';
-    		samplePortfolio['ask']='34';
-    		samplePortfolio['askSize']='34';
-    		samplePortfolio['volume']='34';
-    		samplePortfolio['quantity']='34';
-    		samplePortfolio['crlTotal']='34';
-			samplePortfolio['cashTotal']='34';
-			samplePortfolio['total']='34';
-			$('#portfolio').html(_porfolioTemplate(samplePortfolio));
-
-			$('#orderType').on('change', function() {
-				var option = $(this).val();
-				if (option === 'marketBuy' || option === 'marketSell') $('#orderInputs').html(_orderInputsVolumeTemplate);
-				else $('#orderInputs').html(_orderInputsBothTemplate);
-    		});
-
-    		$('.cancelOrderButton').click(function() {
-    			alert("Are you sure you want to Cancel your order");
-    		});
-
-			$('#submitOrderBtn').click(function() {
-				var time = new Date().getTime();
-
-				//TODO: check if inputs were valid and non empty
-				var orderObject = {};
-				orderObject['time'] = time;
-
-				var volume = $('#volumeInput').val();
-				orderObject['volume'] = volume;
-
-				var option = $('#orderType').val();
-				orderObject['type'] = option;
-				currentOrders[time] = [option, volume];
-
-				if (option === 'limitBuy' || option === 'limitSell') {
-					orderObject['price'] = $('#priceInput').val();
-					currentOrders[time].push($('#priceInput').val());
-				}
-				
-				socket.emit('make order', orderObject);
-				//socket.makeOrder(orderObject);
-				$('input').val('');
-                
-			});
-
+	
 			// socket listens to the server for updates
 			socket.on('update', function(data) {
 				console.log(data);
@@ -99,6 +38,8 @@ $('#submitBtn').click( function() {
 				});
 
 				// enable buttons
+				createTradingPanel();
+
 			});
 
 			// when market closes
@@ -109,3 +50,72 @@ $('#submitBtn').click( function() {
     	});
     }
 });
+
+function createTradingPanel() {
+	// Initialize the trading panel
+	$('.trading').prop('disabled', false);
+    // $('#submitOrderBtn').prop('disabled', false);
+	$('#orderInputs').html(_orderInputsVolumeTemplate);
+	$('#cancelOrders').html(_cancelOrdersTemplate({ orders: ['Buy 13 shares at $324', 'Buy 13 shares at $324', 'Sell 23 shares at $23'] }));
+	generateChartData();
+	makeChart();
+
+	// Get fake transactions
+	$('#transactions').html(_transactionsTemplate({ transactions: ["You sold 100 shares at $2332", "You bought 200 shares at $5", 
+		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
+		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
+		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5",
+		"You sold 100 shares at $2332", "You bought 200 shares at $5", "You sold 100 shares at $2332", "You bought 200 shares at $5"] } ));
+
+	// Set fake portfolio data
+	var samplePortfolio = {}
+	samplePortfolio['last']='34';
+	samplePortfolio['low']='34';
+	samplePortfolio['high']='34';
+	samplePortfolio['bid']='34';
+	samplePortfolio['bidSize']='34';
+	samplePortfolio['ask']='34';
+	samplePortfolio['askSize']='34';
+	samplePortfolio['volume']='34';
+	samplePortfolio['quantity']='34';
+	samplePortfolio['crlTotal']='34';
+	samplePortfolio['cashTotal']='34';
+	samplePortfolio['total']='34';
+	$('#portfolio').html(_porfolioTemplate(samplePortfolio));
+
+	$('#orderType').on('change', function() {
+		var option = $(this).val();
+		if (option === 'marketBuy' || option === 'marketSell') $('#orderInputs').html(_orderInputsVolumeTemplate);
+		else $('#orderInputs').html(_orderInputsBothTemplate);
+	});
+
+	$('.cancelOrderButton').click(function() {
+		alert("Are you sure you want to Cancel your order");
+	});
+
+	$('#submitOrderBtn').click(function() {
+		var time = new Date().getTime();
+
+		//TODO: check if inputs were valid and non empty
+		var orderObject = {};
+		orderObject['time'] = time;
+
+		var volume = $('#volumeInput').val();
+		orderObject['volume'] = volume;
+
+		var option = $('#orderType').val();
+		orderObject['type'] = option;
+		currentOrders[time] = [option, volume];
+
+		if (option === 'limitBuy' || option === 'limitSell') {
+			orderObject['price'] = $('#priceInput').val();
+			currentOrders[time].push($('#priceInput').val());
+		}
+				
+		socket.emit('make order', orderObject);
+		//socket.makeOrder(orderObject);
+		$('input').val('');
+                
+	});
+
+}
