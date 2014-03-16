a6cc5c2c6d5baeaed150c2aa4d2c5718b6ac744b
Stephanie New <syqnew@mit.edu>
2014-03-16 15:52:15 -0400

    Started adding the fulfilling orders logic. NOT TESTED

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.get('/', routes.index);
 app.get('/trader', trader.trade);
 app.get('/admin', admin.admin);
 
+var marketBuyOrders = [];
+var marketSellOrders = [];
+var limitBuyOrders = [];
+var limitSellOrders = [];
+// Queue to stick the orders when they are first coming in, don't know if actually needed. 
+var pendingOrders = [];
+
 io.sockets.on('connection', function (socket) {
     // trader
     // make sure that a nickname is sent first before any requests can be taken 
@@ -35,6 +42,26 @@ io.sockets.on('connection', function (socket) {
             // put in order book
             socket.on('make order', function(obj) {
                 // try to fulfill order from order book
+                console.log(obj);
+
+                // object looks like { time: 1394909156392, volume: '999', type: 'marketBuy' }
+
+                // Handle Market Orders
+                if ( obj['type'] === 'marketBuy' ) {
+                    marketBuyOrders.push(obj);
+                    handleMarketOrder(obj['type']);
+                } else if ( obj['type'] === 'marketSell' ) {
+                    marketSellOrders.push(obj);
+                    handleMarketOrder(obj['type']);
+                } 
+                // Handle Limit Orders
+                else if ( obj['type'] === 'limitBuy' ) {
+                    limitBuyOrders.push(obj);
+                    handleLimitOrder(obj['type']);
+                } else if ( obj['type'] === 'limitSell' ) {
+                    limitSellOrders.push(obj);
+                    handleLimitOrder(obj['type']);
+                } 
                 
                 // transaction object
                 var transObject = {};
@@ -82,6 +109,120 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
+function handleMarketOrder(marketType) {
+    // check if this limit order matches any other market order
+    if ( marketType === 'marketBuy' ) {
+        // match with limit sells
+
+    } else if ( marketType === 'marketSell') {
+        // match with limit buys
+    }
+}
+
+function handleLimitOrder(limitType) {
+    // check if this limit order matches any other limit order
+    if ( limitType === 'limitBuy' ) {
+        // match with limitSells
+
+    } else if ( limitType === 'limitSell') {
+        // match with limitBuys
+
+    }
+
+}
+
+//
+// I might want to do an object wrapper around the orders... 
+//
+
+/**
+ * Market Orders are by ascending time and limit orders by price
+ */
+function matchMarketOrders(marketOrdersList, limitOrdersList, sellAtMarketPrice) {
+    // cannot match if either one of the lists are empty
+    if ( marketOrdersList.length === 0 || limitOrdersList.length === 0) return;
+
+    // first get the order to be matched
+    var firstAtMarket = marketOrdersList[0];
+    var bestAtPrice = limitOrdersList[0];
+
+    var price = bestAtPrice['price'];
+    var amount = firstAtMarket['unfulfilled'];
+    var amountAtPrice = bestAtPrice['unfulfilled'];
+    if ( amount > amountAtPrice ) {
+        amount = amountAtPrice;
+        if ( limitOrdersList.length > 1 ) {
+            var nextOrder = limitOrdersList[1];
+        }
+    }
+
+    var currentTime = firstAtMarket['time'];
+
+    // fulfill the orders (altered in the original list also)
+    bestAtPrice['unfulfilled'] -= amount;
+    firstAtMarket['unfulfilled'] -= amount;
+
+    // record the sale
+    // create a quote
+    // update metadata
+}
+
+/**
+ * 
+ */
+function matchLimitOrders(bidOrders, askOrders, sellInitiated) {
+    // make sure that lists are both nonempty
+    if ( bidOrders.length === 0 || askOrders.length === 0 ) return;
+        
+    var bestBid = bidOrders[0];
+    var bestAsk = askOrders[0];
+
+    var bidPrice = bidOrders['price'];
+    var askPrice = askOrders['price'];
+
+    if ( bidPrice != askPrice ) return;
+
+    var price;
+    var time;
+    if (sellInitiated) {
+        price = bidPrice;
+        time = bestBid['time'];
+    } else {
+        price = askPrice;
+        time = bestAsk['time'];
+    }
+
+    var amount = bestBid['unfulfilled'];
+    var amountAsk = bestAsk['unfulfilled'];
+    if ( amount > amountAsk ) {
+        amount = amountAsk;
+        if ( askOrders.length > 1 ) {
+            var nextAsk = askOrders[1];
+        }
+    } else {
+        if ( amount < amountAsk) {
+            if (bidOrders.length > 1) {
+                var nextBid = bidOrders[1];
+            }
+        } else {
+            if (bidOrders.length > 1) {
+                var nextBid = bidOrders[1];
+            }
+            if ( askOrders.length > 1 ) {
+                var nextAsk = askOrders[1];
+            }
+        }
+    }
+    
+    // fulfill the orders
+    bestBid['unfulfilled'] -= amount;
+    bestAsk['unfulfilled'] -= amount;
+
+    // record sale
+    // update metadata
+    // create quote
+
+}
 
 
 
